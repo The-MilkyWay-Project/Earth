01b8fdee768d21d2ae951cdd18ca7bcddf0f1ef5
Joshua walker <joshua.walker@sykescottages.co.uk>
2020-07-14 21:29:23 +0100

    EARTH Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,5 @@
 <style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
+
 
   h1 {
     font-size: 2.8em;
@@ -13,19 +8,6 @@
     margin: 0 0 0.5em 0;
   }
 
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 auto;
-  }
-
-  p {
-    margin: 1em auto;
-  }
 
   @media (min-width: 480px) {
     h1 {
@@ -35,18 +17,7 @@
 </style>
 
 <svelte:head>
-  <title>Sapper project template</title>
+  <title>Project Template</title>
 </svelte:head>
 
-<h1 class="text-red-400">Great success!</h1>
-
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+<h1 class="text-red-400">Earth</h1>
